64f75d21e35ef5180462f6a6312cd743095492df
Piercen Helms <phelms1999@gmail.com>
2023-03-16 22:21:18 -0700

    fixed bug/renamed some things

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,61 +1,62 @@
+const landingPage = document.querySelector("#landing-page");
+const questionContainer = document.querySelector("#question-container");
+const scoreContainer = document.querySelector("#score-container");
+const leaderboardContainer = document.querySelector("#leaderboard-container");
+const results = document.querySelector("#results");
+const resultsText = document.querySelector("#results-text");
+
 // questions
 const questions = [
     {
-        questionText: "How can a datatype be declared to be a constant type?",
+        Quizquestions: "How can a datatype be declared to be a constant type?",
         options: ["var", "let", "constant", "const"],
         answer: "const",
     },
     {
-        questionText: "JavaScript is an ___ language",
+        Quizquestions: "JavaScript is an ___ language",
         options: ["Object-Oriented", "Object-Based", "Procedural", "None of the above"],
         answer: "Object-Oriented",
     },
     {
-        questionText: "Which of the following keywords is used to define a variable in Javascript?",
+        Quizquestions: "Which of the following keywords is used to define a variable in Javascript?",
         options: ["var", "let", "Both var and let", "None of the above"],
         answer: "Both var and let",
     },
     {
-        questionText: "Which of the following methods is used to access HTML elements using Javascript?",
+        Quizquestions: "Which of the following methods is used to access HTML elements using Javascript?",
         options: ["getElementbyId()", "getElementsByClassName()", "Both a and b", "None of the above"],
         answer: "Both a and b",
     },
     {
-        questionText: "Which of the following methods can be used to display data in some form using Javascript?",
+        Quizquestions: "Which of the following methods can be used to display data in some form using Javascript?",
         options: ["document.write()", "console.log()", "window.alert()", "All of the above"],
         answer: "All of the above",
     },
 ];
 
-const startCard = document.querySelector("#start-card");
-const questionCard = document.querySelector("#question-card");
-const scoreCard = document.querySelector("#score-card");
-const leaderboardCard = document.querySelector("#leaderboard-card");
-
-function hideCards() {
-    startCard.setAttribute("hidden", true);
-    questionCard.setAttribute("hidden", true);
-    scoreCard.setAttribute("hidden", true);
-    leaderboardCard.setAttribute("hidden", true);
+//questions hidden
+function hideContainers() {
+    landingPage.setAttribute("hidden", true);
+    questionContainer.setAttribute("hidden", true);
+    scoreContainer.setAttribute("hidden", true);
+    leaderboardContainer.setAttribute("hidden", true);
 }
-  
-const resultDiv = document.querySelector("#result-div");
-const resultText = document.querySelector("#result-text");
 
-function hideResultText() {
-    resultDiv.style.display = "none";
+function hideTextResults() {
+  results.style.display = "none";
 }
 
 var intervalID;
 var time;
 var currentQuestion;
 
-document.querySelector("#start-button").addEventListener("click", startQuiz);
+//start game
+document.querySelector("#start-btn").addEventListener("click", startGame);
 
-function startQuiz() {
+function startGame() {
     
-    hideCards();
-    questionCard.removeAttribute("hidden");
+  hideContainers();
+    questionContainer.removeAttribute("hidden");
     
     currentQuestion = 0;
     displayQuestion();
@@ -71,7 +72,7 @@ function countdown() {
     time--;
     displayTime();
     if (time < 1) {
-      endQuiz();
+      endGame();
     }
   }
 
@@ -80,42 +81,44 @@ function displayTime() {
   timeDisplay.textContent = time;
 }
 
+//display questions
 function displayQuestion() {
-    let question = questions[currentQuestion];
-    let options = question.options;
+    var question = questions[currentQuestion];
+    var options = question.options;
   
-    let h2QuestionElement = document.querySelector("#question-text");
-    h2QuestionElement.textContent = question.questionText;
+    var QuestionsElement = document.querySelector("#Quizquestions");
+    QuestionsElement.textContent = question.Quizquestions;
   
-    for (let i = 0; i < options.length; i++) {
-      let option = options[i];
-      let optionButton = document.querySelector("#option" + i);
+    for (var i = 0; i < options.length; i++) {
+      var option = options[i];
+      var optionButton = document.querySelector("#option" + i);
       optionButton.textContent = option;
     }
 }
 
-document.querySelector("#quiz-options").addEventListener("click", checkAnswer);
+//check to see if answer is correct or wrong
+document.querySelector("#answer-options").addEventListener("click", checkAnswer);
 
 function optionIsCorrect(optionButton) {
     return optionButton.textContent === questions[currentQuestion].answer;
 }
 
 function checkAnswer(eventObject) {
-    let optionButton = eventObject.target;
-    resultDiv.style.display = "block";
+    var optionButton = eventObject.target;
+    results.style.display = "block";
     if (optionIsCorrect(optionButton)) {
-      resultText.textContent = "Correct!";
-      setTimeout(hideResultText, 1000);
+      resultsText.textContent = "Correct!";
+      setTimeout(hideTextResults, 1000);
     } else {
-      resultText.textContent = "Incorrect!";
-      setTimeout(hideResultText, 1000);
+      resultsText.textContent = "Incorrect!";
+      setTimeout(hideTextResults, 1000);
       if (time >= 5) {
         time = time - 5;
         displayTime();
       } else {
         time = 0;
         displayTime();
-        endQuiz();
+        endGame();
       }
     }
     
@@ -123,23 +126,25 @@ function checkAnswer(eventObject) {
     if (currentQuestion < questions.length) {
         displayQuestion();
       } else {
-        endQuiz();
+        endGame();
       }
 }
 
+// score
 const score = document.querySelector("#score");
 
-function endQuiz() {
+//end game
+function endGame() {
     clearInterval(intervalID);
-    hideCards();
-    scoreCard.removeAttribute("hidden");
+    hideContainers();
+    scoreContainer.removeAttribute("hidden");
     score.textContent = time;
 }
   
-const submitButton = document.querySelector("#submit-button");
+const submitBtn = document.querySelector("#submit-btn");
 const inputElement = document.querySelector("#initials");
 
-submitButton.addEventListener("click", storeScore);
+submitBtn.addEventListener("click", storeScore);
 
 function storeScore(event) {
     
@@ -150,29 +155,30 @@ function storeScore(event) {
       return;
     }
   
-    let leaderboardItem = {
+    var leaderboardItem = {
       initials: inputElement.value,
       score: time,
     };
   
     updateStoredLeaderboard(leaderboardItem);
   
-    hideCards();
-    leaderboardCard.removeAttribute("hidden");
+    hideContainers();
+    leaderboardContainer.removeAttribute("hidden");
   
     renderLeaderboard();
 }
 
+//update leaderboards
 function updateStoredLeaderboard(leaderboardItem) {
-    let leaderboardArray = getLeaderboard();
+    var leaderboardArray = getLeaderboard();
     leaderboardArray.push(leaderboardItem);
     localStorage.setItem("leaderboardArray", JSON.stringify(leaderboardArray));
 }
 
 function getLeaderboard() {
-    let storedLeaderboard = localStorage.getItem("leaderboardArray");
+    var storedLeaderboard = localStorage.getItem("leaderboardArray");
     if (storedLeaderboard !== null) {
-      let leaderboardArray = JSON.parse(storedLeaderboard);
+      var leaderboardArray = JSON.parse(storedLeaderboard);
       return leaderboardArray;
     } else {
       leaderboardArray = [];
@@ -181,12 +187,12 @@ function getLeaderboard() {
 }
 
 function renderLeaderboard() {
-    let sortedLeaderboardArray = sortLeaderboard();
+    var sortedLeaderboardArray = sortLeaderboard();
     const highscoreList = document.querySelector("#highscore-list");
     highscoreList.innerHTML = "";
     for (let i = 0; i < sortedLeaderboardArray.length; i++) {
-      let leaderboardEntry = sortedLeaderboardArray[i];
-      let newListItem = document.createElement("li");
+      var leaderboardEntry = sortedLeaderboardArray[i];
+      var newListItem = document.createElement("li");
       newListItem.textContent =
         leaderboardEntry.initials + " - " + leaderboardEntry.score;
       highscoreList.append(newListItem);
@@ -194,7 +200,7 @@ function renderLeaderboard() {
 }
 
 function sortLeaderboard() {
-    let leaderboardArray = getLeaderboard();
+    var leaderboardArray = getLeaderboard();
     if (!leaderboardArray) {
       return;
     }
@@ -204,29 +210,31 @@ function sortLeaderboard() {
     });
     return leaderboardArray;
 }
-  
-const clearButton = document.querySelector("#clear-button");
-clearButton.addEventListener("click", clearHighscores);
+
+//clear leaderboards
+const clearBtn = document.querySelector("#clear-btn");
+clearBtn.addEventListener("click", clearHighscores);
 
 function clearHighscores() {
     localStorage.clear();
     renderLeaderboard();
 }
-  
-const backButton = document.querySelector("#back-button");
-backButton.addEventListener("click", returnToStart);
+
+//back to main screen
+const backBtn = document.querySelector("#back-btn");
+backBtn.addEventListener("click", returnToStart);
 
 function returnToStart() {
-    hideCards();
-    startCard.removeAttribute("hidden");
+    hideContainers();
+    landingPage.removeAttribute("hidden");
 }
 
 const leaderboardLink = document.querySelector("#leaderboard-link");
 leaderboardLink.addEventListener("click", showLeaderboard);
 
 function showLeaderboard() {
-  hideCards();
-  leaderboardCard.removeAttribute("hidden");
+  hideContainers();
+  leaderboardContainer.removeAttribute("hidden");
   clearInterval(intervalID);
   time = undefined;
   displayTime();
